81e28bf0900de561c90c0d94b85ce1a6b17674c5
Nakul Upasani <69073624+nyu19-py@users.noreply.github.com>
2021-09-23 15:32:30 +0530

    new cb

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ const axios = require('axios')
 function jokeRequested(){
     let targetText = document.getElementById("output")
     targetText.textContent = "Hello"
-    let req_url
-
+    let base_url="https://v2.jokeapi.dev/joke/"
+    
     return axios.get(){
 
     }
+}
+
+function enabler(){
+    console.log("pk")
+    document.getElementById("joke-prog").disabled=false
+    document.getElementById("joke-misc").disabled=false
+    document.getElementById("joke-dark").disabled=false
+    document.getElementById("joke-puns").disabled=false
+    document.getElementById("joke-spooky").disabled=false
+}
+
+function disabler(){
+    console.log("pk")
+    document.getElementById("joke-prog").disabled=true
+    document.getElementById("joke-misc").disabled=true
+    document.getElementById("joke-dark").disabled=true
+    document.getElementById("joke-puns").disabled=true
+    document.getElementById("joke-spooky").disabled=true
 }
